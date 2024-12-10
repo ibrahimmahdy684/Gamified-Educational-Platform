3eabae03d938c63cea3cb58e4510b98860351be9
Ibrahim Elmahdy <ibrahim.elmahdy@Student.giu-uni.de>
2024-12-10 19:13:49 +0200

    Add files via upload

---
+++ GamifiedPlatform/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YourNamespace.Models
+{
+    // This defines the User model that corresponds to the Users table
+    [Table("Users")]  // Optional if your table name matches the class name
+    public class User
+    {
+        [Key]  // Specifies that this is the primary key
+        public int UserID { get; set; }
+
+        [Required]  // Specifies that Name is a required field
+        [StringLength(100)]  // Optional: Set a maximum length for the Name field
+        public string Name { get; set; }
+
+        [Required]  // Specifies that Email is a required field
+        [StringLength(100)]  // Optional: Set a maximum length for the Email field
+        [EmailAddress]  // Ensures the value is a valid email format
+        public string Email { get; set; }
+
+        [StringLength(15)]  // Optional: Set a maximum length for the Phone field
+        public string Phone { get; set; }
+
+        [StringLength(255)]  // Optional: Set a maximum length for the Address field
+        public string Address { get; set; }
+    }
+}
