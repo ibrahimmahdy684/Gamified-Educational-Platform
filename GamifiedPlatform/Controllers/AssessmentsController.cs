1b872c14c4cae1122d341b1712b7a62e2bb7cefd
Maher1506 <68017554+Maher1506@users.noreply.github.com>
2024-12-10 11:26:36 +0200

    Created Assesments and Courses Controllers

---
+++ GamifiedPlatform/Controllers/AssessmentsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GamifiedPlatform.Models;
+
+namespace GamifiedPlatform.Controllers
+{
+    public class AssessmentsController : Controller
+    {
+        private readonly GamifiedPlatformContext _context;
+
+        public AssessmentsController(GamifiedPlatformContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Assessments
+        public async Task<IActionResult> Index()
+        {
+            var gamifiedPlatformContext = _context.Assessments.Include(a => a.Module);
+            return View(await gamifiedPlatformContext.ToListAsync());
+        }
+
+        // GET: Assessments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var assessment = await _context.Assessments
+                .Include(a => a.Module)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (assessment == null)
+            {
+                return NotFound();
+            }
+
+            return View(assessment);
+        }
+
+        // GET: Assessments/Create
+        public IActionResult Create()
+        {
+            ViewData["ModuleId"] = new SelectList(_context.Modules, "ModuleId", "ModuleId");
+            return View();
+        }
+
+        // POST: Assessments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ModuleId,CourseId,Type,TotalMarks,PassingMarks,Criteria,Weightage,Description,Title")] Assessment assessment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(assessment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ModuleId"] = new SelectList(_context.Modules, "ModuleId", "ModuleId", assessment.ModuleId);
+            return View(assessment);
+        }
+
+        // GET: Assessments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var assessment = await _context.Assessments.FindAsync(id);
+            if (assessment == null)
+            {
+                return NotFound();
+            }
+            ViewData["ModuleId"] = new SelectList(_context.Modules, "ModuleId", "ModuleId", assessment.ModuleId);
+            return View(assessment);
+        }
+
+        // POST: Assessments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ModuleId,CourseId,Type,TotalMarks,PassingMarks,Criteria,Weightage,Description,Title")] Assessment assessment)
+        {
+            if (id != assessment.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(assessment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AssessmentExists(assessment.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ModuleId"] = new SelectList(_context.Modules, "ModuleId", "ModuleId", assessment.ModuleId);
+            return View(assessment);
+        }
+
+        // GET: Assessments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var assessment = await _context.Assessments
+                .Include(a => a.Module)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (assessment == null)
+            {
+                return NotFound();
+            }
+
+            return View(assessment);
+        }
+
+        // POST: Assessments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var assessment = await _context.Assessments.FindAsync(id);
+            if (assessment != null)
+            {
+                _context.Assessments.Remove(assessment);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AssessmentExists(int id)
+        {
+            return _context.Assessments.Any(e => e.Id == id);
+        }
+    }
+}
