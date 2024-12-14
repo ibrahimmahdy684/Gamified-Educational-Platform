60084546bfca55ed8e68007c5499339c2c872c57
Maher1506 <68017554+Maher1506@users.noreply.github.com>
2024-12-14 20:24:28 +0200

    minor debud

--- GamifiedPlatform/Controllers/LearnersController.cs
+++ GamifiedPlatform/Controllers/LearnersController.cs
@@ -160,16 +160,14 @@ namespace GamifiedPlatform.Controllers
             }
             return View(learner);
         }
-        
-        // GET: Learners/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var learner = await _context.Learners.FindAsync(id);
+
+
+        // GET: Learners/Edit/5
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var learner = _context.Learners.FirstOrDefault(l => l.LearnerId == id);
             if (learner == null)
             {
                 return NotFound();
@@ -177,40 +175,24 @@ namespace GamifiedPlatform.Controllers
             return View(learner);
         }
 
-        // POST: Learners/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // This action will save the edited profile
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("LearnerId,FirstName,LastName,Gender,BirthDate,Country,CulturalBackground")] Learner learner)
+        public IActionResult Edit(Learner learner)
         {
-            if (id != learner.LearnerId)
-            {
-                return NotFound();
-            }
-
             if (ModelState.IsValid)
             {
-                try
-                {
-                    _context.Update(learner);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!LearnerExists(learner.LearnerId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                // Save the learner details to the database
+                _context.Update(learner);
+                _context.SaveChanges();
+
+                // Redirect to the profile page after saving
+                return RedirectToAction("Profile", new { id = learner.LearnerId });
             }
+
+            // If the model state is invalid, return the view with the model data so that validation errors are shown
             return View(learner);
         }
+
 
         // GET: Learners/Delete/5
         public async Task<IActionResult> Delete(int? id)
