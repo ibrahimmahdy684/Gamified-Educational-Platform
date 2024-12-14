b2671ffc973079ffe233788b599106eb4bf0249a
Ibrahim Elmahdy <ibrahim.elmahdy@Student.giu-uni.de>
2024-12-14 20:34:53 +0200

    Update Program.cs

--- GamifiedPlatform/Program.cs
+++ GamifiedPlatform/Program.cs
@@ -1,35 +1,15 @@
 using GamifiedPlatform.Models;
 using Microsoft.EntityFrameworkCore;
+using GamifiedPlatform.Models;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-// Add services to the container
-builder.Services.AddAuthentication("CookieAuth")
-    .AddCookie("CookieAuth", options =>
-    {
-        options.LoginPath = "/Account/Login"; // Redirect here for unauthorized access
-    });
-
-builder.Services.AddAuthorization(); // Add authorization middleware
-
-var app = builder.Build();
-
-// Add authentication and authorization middlewares
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddDbContext<GamifiedPlatformContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
